a445c59b2882488deb5fa967eb278c9e96add592
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-02-09 01:56:50 +0100

    done fi now

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+const { createServer } = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require('cors');
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
+
+const app = express();
+const server = createServer(app);
+const PORT = 3000
+
+
+// Create a separate cors instance for Socket.IO
+const ioCors = cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+});
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+    ioCors(req, res, (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error handling CORS' });
+        }
+        next();
+    });
+});
+
+const io = new Server(server, {
     cors: {
-        origin: "https://example.com"
+        origin: "*",
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+
     }
 });
 
-io.on("connection", (socket) => {
+io.of("/socket").on("connection", (socket) => {
     console.log("Connection Successful")
     console.log(socket.id)
+    socket.on("salutations", (greeting) => {
+        console.log(greeting);
+    })
 });
 
-io.listen(3000)
+
+server.listen(PORT, () => {
+    console.log("listening on port 3000")
+})
